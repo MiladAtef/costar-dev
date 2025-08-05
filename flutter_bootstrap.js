71570cff833b8b33f05af54d68fabb2e4cc93df4
Milad <meladattef2017@gmail.com>
2025-08-05 23:28:32 +0300

    Deploy version 1.0.0+85

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3938046563"
+    serviceWorkerVersion: "2781519429"
   }
 });
